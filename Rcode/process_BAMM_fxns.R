4ee40263ed4a4ea5e406976fb73c8370361c7a19
pascaltitle <pascaltitle@gmail.com>
2013-11-18 20:41:35 -0500

    Add plotRateThroughTime function as well as transparentColor function

--- Rcode/process_BAMM_fxns.R
+++ Rcode/process_BAMM_fxns.R
@@ -888,8 +888,131 @@ getPathToRoot <- function(phy, node){
 }
 
 
+#############################################################
+#
+#	transparentColor <- function(...)
+#
+#	Internal function allows for defining named colors with opacity
+#	alpha = opacity
+#
+transparentColor<-function(namedColor,alpha=0.8){
+	res<-c(as.vector(col2rgb(namedColor))/255,alpha);
+	return(rgb(red=res[1],green=res[2],blue=res[3],alpha=res[4]));
+}
 
 
+#############################################################
+#
+# plotRateThroughTime <- function(...)
+#
+# useMedian = boolean, will plot median if TRUE, mean if FALSE.
+# intervals if NULL, no intervals will be plotted, otherwise a vector of quantiles must be supplied (these will define shaded polygons)
+# ratetype = 'speciation' or 'extinction' or 'netdiv' or 'trait'
+# nBins = number of time slices used to generate rates through time
+# smooth = boolean whether or not to apply loess smoothing
+# smoothParam = loess smoothing parameter, ignored if smooth = F
+# opacity = opacity of color for interval polygons
+# intervalCol = transparent color for interval polygons
+# avgCol = color for mean/median line
+#
+plotRateThroughTime <- function(ephy, useMedian = F, intervals=seq(from = 0,to = 1,by = 0.01), ratetype = 'speciation', nBins = 100, smooth=F, smoothParam = 0.20, opacity = 0.1,intervalCol='blue', avgCol='red'){
+	
+	if (!'bamm-data' %in% class(ephy)){
+		stop("Object ephy must be of class bamm-data\n");
+	}
+	if (!is.logical(useMedian)){
+		stop('ERROR: useMedian must be either TRUE or FALSE.');
+	}
+	if (class(intervals)!='numeric' & class(intervals)!='NULL'){
+		stop("ERROR: intervals must be either 'NULL' or a vector of quantiles.");
+	}
+
+	#get rates through binned time
+	rmat <- getRateThroughTimeMatrix(ephy, nslices = nBins);
+
+	#set appropriate rates
+	if (ratetype != 'speciation' & ratetype != 'extinction' & ratetype != 'netdiv' & ratetype != 'trait'){
+		stop("ERROR: ratetype must be 'speciation', 'extinction', 'netdiv' or 'trait'.\n");
+	}
+	if (ratetype == 'speciation'){
+		rate <- rmat$lambda;
+		ratelabel <- 'Speciation';
+	}
+	if (ratetype == 'trait'){
+		rate <- rmat$lambda;
+		ratelabel <- 'BM rate';
+	}
+
+	if (ratetype == 'extinction'){
+		rate <- rmat$mu;
+		ratelabel <- 'Extinction';
+	}
+	if (ratetype == 'netdiv'){
+		rate <- rmat$lambda - rmat$mu;
+		ratelabel <- 'Net diversification';
+	}
+
+	#generate coordinates for polygons
+	maxTime <- max(rmat$times);
+	if (!is.null(intervals)){
+		mm <- apply(rate, MARGIN = 2, quantile, intervals);
+
+		poly<-list();
+		q1<-1;
+		q2<-nrow(mm);
+		repeat{
+			if (q1 >= q2) {break}
+			a<-as.data.frame(cbind(rmat$times,mm[q1,]));
+			b<-as.data.frame(cbind(rmat$times,mm[q2,]));
+			b<-b[rev(rownames(b)),];
+			colnames(a)<-colnames(b)<-c('x','y');
+			poly[[q1]]<-rbind(a,b);
+			q1<-q1+1;
+			q2<-q2-1;
+		}
+	}
+
+	#Calculate averaged data line
+	if (useMedian == F){
+		avg <- colMeans(rate);
+	}
+	if (useMedian == T){
+		avg <- unlist(apply(rate,2,median));
+	}
+	
+	#apply loess smoothing to intervals
+	if (smooth == T){
+		for (i in 1:length(poly)){
+			poly[[i]][1:nrow(poly[[i]])/2,2] <- loess(poly[[i]][1:nrow(poly[[i]])/2,2] ~ poly[[i]][1:nrow(poly[[i]])/2,1],span = smoothParam)$fitted;
+			poly[[i]][(nrow(poly[[i]])/2):nrow(poly[[i]]),2] <- loess(poly[[i]][(nrow(poly[[i]])/2):nrow(poly[[i]]),2] ~ poly[[i]][(nrow(poly[[i]])/2):nrow(poly[[i]]),1],span = smoothParam)$fitted;
+		}
+		avg <- loess(avg ~ rmat$time,span = smoothParam)$fitted;
+	}
+
+	#begin plotting
+	plot.new();
+	par(oma=c(1,1,1,1));
+	par(mar=c(6,6,1,1));
+	plot.window(xlim=c(maxTime, 0), ylim=c(0 , max(rate) + 0.2*max(rate)));
+
+	#plot intervals
+	if (!is.null(intervals)){
+		for (i in 1:length(poly)){
+			polygon(x=maxTime - poly[[i]][,1],y=poly[[i]][,2],col=transparentColor(intervalCol,opacity),border=NA);
+		}
+	}
+	if (useMedian == F){
+		lines(x = maxTime - rmat$time, y = avg, lwd = 3, col = avgCol);
+	}
+	if (useMedian == T){
+		lines(x = maxTime - rmat$time, y = avg, lwd = 3, col = avgCol);
+	}
+	
+	axis(at=seq(0, maxTime + 0.3*maxTime, by = 5), cex.axis = 1.2, side = 1);
+	axis(at=seq(-0.2, max(rate) + 0.2*max(rate), by=0.1), las=1, cex.axis = 1.2, side = 2);
+	mtext(side = 1, text = 'Time since present', line = 3, cex = 1.3);
+	mtext(side = 2, text = ratelabel, line = 3, cex = 1.3);
+}
 
 
 
