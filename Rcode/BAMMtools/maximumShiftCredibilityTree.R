c7cbc89b1235e4f804e1b28f3fa69371fcd9aaf4
Pascal Title <pascaltitle@gmail.com>
2013-11-22 21:28:51 -0500

    Separate functions into separate script files.

--- Rcode/BAMMtools/maximumShiftCredibilityTree.R
+++ Rcode/BAMMtools/maximumShiftCredibilityTree.R
@@ -83,10 +83,3 @@ maximumShiftCredibilityTree <- function(ephy, maximize = 'product'){
  	obj$sampleindex <- sampleindex;
 	return(obj);
 }
-
-areEventConfigurationsIdentical <- function(ephy, index1, index2){
-	
-	nodeset <- c(ephy$eventData[[index1]]$node, ephy$eventData[[index2]]$node);
-	diffs <- sum(table(nodeset) == 1);
-	return(diffs == 0);	
-}
