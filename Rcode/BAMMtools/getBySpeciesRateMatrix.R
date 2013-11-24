e8e33ed34c0392f02c455531fd212256cef82e22
Pascal Title <pascaltitle@gmail.com>
2013-11-24 16:49:48 -0500

    Adjust getSpeciesRateThroughTime.R to accept BAMMtrait data, as well as add ability to use the node argument that was present but not functional

--- Rcode/BAMMtools/getBySpeciesRateMatrix.R
+++ Rcode/BAMMtools/getBySpeciesRateMatrix.R
@@ -4,22 +4,28 @@
 #
 #	Start time: how many time units before present to include
 #	nbreaks: how many time points to compute the rate
+#	ndr: boolean, should net diversification rate be returned, ignored if ephy is of type "traits".
+#	node: if NULL, all species returned, if defined, species descending from that node are returned. 
 #
 #	This version computes for all species
 #	
-#	node argument will just compute for the subtree descended from "node"
 
-getBySpeciesRateMatrix <- function(ephy, start.time, nbreaks, ndr=TRUE, node){
+
+getBySpeciesRateMatrix <- function(ephy, start.time, nbreaks, ndr=TRUE, node=NULL){
 	
-	
-	spset <- ephy$tip.label;
-	
-	mm <- matrix(NA, nrow=length(spset), ncol=nbreaks);
-	for (i in 1:nrow(mm)){
-		#cat(spset[i], '\n')
-		mm[i,] <- getSpeciesRateThroughTime(ephy, start.time, nbreaks, ndr, species=spset[i]);
+	if (!is.null(node)){
+		if (node >= Ntip(ephy)){
+			spset <- tips(ephy,node)
+		}
+	} else{
+		spset <- ephy$tip.label;
 	}
 	
-	rownames(mm) <- spset;
+	if (ephy$type == 'traits'){
+		ndr <- FALSE
+	}
+	
+	mm <- t(sapply(spset,function(x) getSpeciesRateThroughTime(ephy,start.time, nbreaks, ndr, species=x)))
+	
 	return(mm);
 }
