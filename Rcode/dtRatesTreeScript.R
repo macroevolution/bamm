79c56b10cfb9b9dd1c3d40fe9156a868c48fb56f
Mike Grundler <mcgrundler@gmail.com>
2013-11-25 15:01:49 -0500

    Add functionality to nonpolar trees with dynamic rates

--- Rcode/dtRatesTreeScript.R
+++ Rcode/dtRatesTreeScript.R
@@ -7,4 +7,5 @@ fn = 'beventdata.txt';
 x = getEventData(phy, fn, burnin = 0.5);
 x = dtRates(x, 0.01);
 
-polartree(x,5,lwd=3,colorize=TRUE);
+plot.dtrates(x,method='phylogram',lwd=3,palette='temperature',ncolors=64);
+plot.dtrates(x,method='polar',lwd=3,vtheta=5,rbf=0.001,palette='temperature',ncolors=64);
