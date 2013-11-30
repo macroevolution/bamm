fee1482019d0d648ebbc0cfda5bc55885ca2e097
Dan Rabosky <dan.rabosky@gmail.com>
2013-11-30 08:57:54 -0500

    Revert to old getEventData, minor bug to fix but no time now

--- Rcode/BAMMtools/getEventData.R
+++ Rcode/BAMMtools/getEventData.R
@@ -62,50 +62,6 @@ getEventData <- function(phy, eventfilename, burnin=0, nsamples = NULL, verbose=
  		phy <- getRecursiveSequence(phy);
  	cat('\nDone with recursive sequence\n\n');
  
-	######### Get ancestors for each unique pair of taxa
-	if (verbose){
-		cat("Start preprocessing unique MRCA pairs....\n")
-	}	
-		
-	x2 <- x[x$generation %in% goodsamples, ];
-	
-	x2 <- x2[!is.na(x2$leftchild) & !is.na(x2$rightchild), ];
-	
-	ff <- character(nrow(x2));
-	
-	for (i in 1:length(ff)){
-		ff[i] <- paste(x2$leftchild[i], x2$rightchild[i], sep='////');
-	}
-	ff <- unique(ff);
-	uniquePairSet <- matrix(NA, nrow=length(ff), ncol=2);
-	
-	uniquePairNode <- numeric(length(ff));
-	
-	for (i in 1:length(ff)){
-		
-		tax <- unlist(strsplit(ff[i], '////'));
-		if (sum(is.na(tax)) == 0){
-			uniquePairSet[i,1] <- which(phy$tip.label == tax[1]);
-			uniquePairSet[i,2] <- which(phy$tip.label == tax[2]);
-			uniquePairNode[i] <- getMRCA(phy, tip=uniquePairSet[i,]);			
-		}
-		
-	}
-	
-	if (verbose){
-		cat("Done preprocessing unique MRCA pairs....\n")
-	}	
-	
-	testNodesFunction <- function(x, y){
-		bb <- FALSE;
-		if (sum (x %in% y) == 2){
-			bb <- TRUE;
-		}
-		return(bb);
-	}
- 
-	####### Done with risky sstuff
-	
 	meanTipMu <- numeric(length(phy$tip.label));
 	
  	meanTipLambda <- numeric(length(phy$tip.label)); 
@@ -123,11 +79,10 @@ getEventData <- function(phy, eventfilename, burnin=0, nsamples = NULL, verbose=
  		lam1 <- tmpEvents[,5]; # lambda parameter 1
  		lam2 <- tmpEvents[,6]; # lambda parameter 2
  		if(type == 'diversification'){
-			
 			mu1 <- tmpEvents[, 7]; # mu parameter 1
  			mu2 <- tmpEvents[, 8]; #mu parameter 2 
-		} else { #for bamm trait data we set the mu columns to zero because those params don't exist
-			
+		}
+		else{ #for bamm trait data we set the mu columns to zero because those params don't exist
 			mu1 <- rep(0, nrow(tmpEvents)); 
  			mu2 <- rep(0, nrow(tmpEvents)); 
 		}
@@ -136,39 +91,15 @@ getEventData <- function(phy, eventfilename, burnin=0, nsamples = NULL, verbose=
  		
  		# Get subtending node for each event:
  		nodeVec <- numeric(nrow(tmpEvents));
-
- 		# This is faster??:
  		for (k in 1:length(nodeVec)){
  			if (is.na(t2[k])){
  				# Node is a tip
  				nodeVec[k] <- which(phy$tip.label == t1[k]);	
  			}else{
- 				tipnode1 <- which(phy$tip.label == t1[k]);
- 				tipnode2 <- which(phy$tip.label == t2[k]);
- 				
- 				lv <- apply(uniquePairSet,  MARGIN=1, testNodesFunction, y=c(tipnode1, tipnode2));
-
-				if (sum(lv) == 1){
-					nodeVec[k] <- uniquePairNode[lv];
-				}else{
- 					tipnodes <- c(which(phy$tip.label == t1[k]), which(phy$tip.label == t2[k]));
- 					nodeVec[k] <- getMRCA(phy, tipnodes);					
-				}
- 				
-
+ 				tipnodes <- c(which(phy$tip.label == t1[k]), which(phy$tip.label == t2[k]));
+ 				nodeVec[k] <- getMRCA(phy, tipnodes);
  			}
  		}
- 
- 		# # This is slow:
- 		# for (k in 1:length(nodeVec)){
- 			# if (is.na(t2[k])){
- 				# # Node is a tip
- 				# nodeVec[k] <- which(phy$tip.label == t1[k]);	
- 			# }else{
- 				# tipnodes <- c(which(phy$tip.label == t1[k]), which(phy$tip.label == t2[k]));
- 				# nodeVec[k] <- getMRCA(phy, tipnodes);
- 			# }
- 		# }
  		
  		if (sum(nodeVec == 0)  > 0){
 			stop('Failed to assign event to node\n');
@@ -208,23 +139,18 @@ getEventData <- function(phy, eventfilename, burnin=0, nsamples = NULL, verbose=
 		}
 
  		tmpEventSegMat <- matrix(0, nrow=(max(phy$edge) + nrow(dftemp) - 2), ncol=4);
- 		
-		
 		non.root <- c(1:length(phy$tip.label), (length(phy$tip.label)+2):max(phy$edge));
 		pos <- 1;	
 		
-		is_noEventBranch <- ! (phy$edge[,2] %in% dftemp$node);
-		
-		tmpEventSegMat[1:sum(is_noEventBranch), 1] <- phy$edge[,2][is_noEventBranch];
-		tmpEventSegMat[1:sum(is_noEventBranch),2] <- tphy$begin[is_noEventBranch];
- 		tmpEventSegMat[1:sum(is_noEventBranch),3] <- tphy$end[is_noEventBranch];
- 		tmpEventSegMat[1:sum(is_noEventBranch),4] <- tphy$statevec[is_noEventBranch];		
- 		
-		eventnodeset <- intersect(non.root, dftemp$node);
-		pos <- 1 + sum(is_noEventBranch);
-		for (k in eventnodeset){
-			
-				events.on.branch <- dftemp[dftemp$node == k, ];
+		for (k in non.root){
+ 			events.on.branch <- dftemp[dftemp$node == k, ];
+			if (nrow(events.on.branch) == 0){
+				tmpEventSegMat[pos,1] <- k;
+				tmpEventSegMat[pos,2] <- tphy$begin[tphy$edge[,2] == k];
+				tmpEventSegMat[pos,3] <- tphy$end[tphy$edge[,2] == k];
+				tmpEventSegMat[pos,4] <- tphy$statevec[tphy$edge[,2] == k];
+				
+			}else{
 				events.on.branch <- events.on.branch[order(events.on.branch$time), ];
 				
 				fBranch <- phy$edge[,2] == k;
@@ -243,45 +169,12 @@ getEventData <- function(phy, eventfilename, burnin=0, nsamples = NULL, verbose=
 				tmpEventSegMat[zzindex, 1] <- rep(k, length(zzindex));
 				tmpEventSegMat[zzindex, 2] <- start.times;
 				tmpEventSegMat[zzindex, 3] <- stop.times;
-				tmpEventSegMat[zzindex, 4] <- proc.set;		
-				pos <- pos + 1 + nrow(events.on.branch);
-		}
-		
-		
-		# Old way. This is vastly slower than new code above.	
-		# for (k in non.root){
- 			# events.on.branch <- dftemp[dftemp$node == k, ];
-			# if (nrow(events.on.branch) == 0){
-				# tmpEventSegMat[pos,1] <- k;
-				# tmpEventSegMat[pos,2] <- tphy$begin[tphy$edge[,2] == k];
-				# tmpEventSegMat[pos,3] <- tphy$end[tphy$edge[,2] == k];
-				# tmpEventSegMat[pos,4] <- tphy$statevec[tphy$edge[,2] == k];
+				tmpEventSegMat[zzindex, 4] <- proc.set;
 				
-			# }else{
-				# events.on.branch <- events.on.branch[order(events.on.branch$time), ];
-				
-				# fBranch <- phy$edge[,2] == k;
- 				# start.times <- c(phy$begin[fBranch], events.on.branch$time);
-				# stop.times <- c(events.on.branch$time, phy$end[fBranch]);
-				# parent <- phy$edge[,1][phy$edge[,2] == k];
-				# if (parent == (length(phy$tip.label) + 1)){
-					# # Parent is root:
-					# proc.set <- c(1, events.on.branch$index);	
-				# }else{
-					# proc.set <- c(tphy$statevec[tphy$edge[,2] == parent], events.on.branch$index);			
-				# }
-				
- 				# zzindex <- pos:(pos+nrow(events.on.branch));	
-				
-				# tmpEventSegMat[zzindex, 1] <- rep(k, length(zzindex));
-				# tmpEventSegMat[zzindex, 2] <- start.times;
-				# tmpEventSegMat[zzindex, 3] <- stop.times;
-				# tmpEventSegMat[zzindex, 4] <- proc.set;
-				
-			# }
+			}
 			
-			# pos <- pos + 1 + nrow(events.on.branch);
-		# } 	
+			pos <- pos + 1 + nrow(events.on.branch);
+		} 	
  	
  		eventBranchSegs[[i]] <- tmpEventSegMat;
 
