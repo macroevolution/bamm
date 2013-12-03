416bfc98b6e6bd975195c7d151d8f5b2bc321f17
blueraleigh <mcgrundler@gmail.com>
2013-12-02 23:42:02 -0500

    Add src directory for C functions called by R

---
+++ Rcode/src/dtrates.c
@@ -0,0 +1,186 @@
+#include <R.h>
+#include <Rinternals.h>
+
+//forward function declarations
+SEXP getListElement(SEXP list, char *str);
+SEXP dtrates(SEXP ephy, SEXP segmat, SEXP tol, SEXP sample);
+SEXP getMatrixColumn(SEXP matrix, int col);
+double getMeanRateExponential(double t1, double t2, double p1, double p2);
+double getTimeIntegratedBranchRate(double t1, double t2, double p1, double p2);
+
+SEXP getListElement(SEXP list, char *str)
+{
+	SEXP elmt = R_NilValue, names = getAttrib(list, R_NamesSymbol);
+	int i;
+	for (i = 0; i < LENGTH(list); i++)
+	{
+		if(strcmp(CHAR(STRING_ELT(names, i)), str) == 0)
+		{
+			elmt = VECTOR_ELT(list, i);
+			break;
+		}
+	}
+	return elmt;
+}
+
+SEXP getMatrixColumn(SEXP matrix, int col)
+{
+	int nrow = INTEGER(getAttrib(matrix, R_DimSymbol))[0];
+	
+	SEXP ret;
+	ret = PROTECT(allocVector(REALSXP, nrow));
+	
+	int i;
+	for (i = 0; i < nrow; i++)
+	{
+		REAL(ret)[i] = REAL(matrix)[i + nrow * col];
+	}
+	return ret;
+}
+
+double getMeanRateExponential(double t1, double t2, double p1, double p2)
+{	
+	if (p2 == 0) 
+	{
+		return p1;
+	}
+	return (p1/p2)*(exp(t2*p2) - exp(t1*p2))/(t2 - t1);
+}
+
+double getTimeIntegratedBranchRate(double t1, double t2, double p1, double p2)
+{
+	if (p2 == 0) 
+	{
+		return (t2 - t1) * p1;
+	}
+	return (p1/p2)*(exp(t2*p2) - exp(t1*p2));
+}
+
+/***
+ephy is the bammdata object, a list holding all the relevant data.
+segmat is a matrix where the rows represent branches of a phylogeny
+broken up into many small segments.  each row indexes one such segment.
+columns 2 and 3 give the starting and ending times of that segment and
+column 1 is the node of the phylogeny to which that segement belongs.
+tol is a precision parameter used for comparing starting and ending 
+times of approximating segments and starting and ending times of branches
+or branch segments on the phylogeny.
+***/
+
+SEXP dtrates(SEXP ephy, SEXP segmat, SEXP tol, SEXP sample)
+{
+	double eps = REAL(tol)[0];
+	
+	int k, j, l, nprotect = 0;
+	//int nsamples = LENGTH(getListElement(ephy, "eventBranchSegs"));
+	int nsamples = LENGTH(sample);
+	
+	SEXP nodeseg = getMatrixColumn(segmat, 0); nprotect++;	
+	SEXP segbegin = getMatrixColumn(segmat, 1); nprotect++;
+	SEXP segend = getMatrixColumn(segmat, 2); nprotect++;
+	int nsegs = LENGTH(nodeseg);
+	
+	SEXP rates;
+	PROTECT(rates = allocVector(REALSXP, nsegs)); nprotect++;
+	
+	for(k = 0; k < nsegs; k++)
+	{
+		REAL(rates)[k] = 0.;
+	}
+	
+	int nrow, node, nnode, event, nxtevent, isGoodStart, isGoodEnd, place_holder;
+	double begin, end, Start, lam1, lam2, relStart, relEnd, rightshift, leftshift, ret;		
+	//for (k = 0; k < nsamples; k++)
+	for (k = INTEGER(sample)[0] - 1; k < INTEGER(sample)[nsamples - 1]; k++)
+	{
+		SEXP eventSegs, eventData;
+		
+		eventSegs = PROTECT(VECTOR_ELT(getListElement(ephy, "eventBranchSegs"), k)); nprotect++;
+		eventData = PROTECT(VECTOR_ELT(getListElement(ephy, "eventData"), k)); nprotect++;
+		
+				
+		nrow = INTEGER(getAttrib(eventSegs, R_DimSymbol))[0];
+		place_holder = 0;
+		for(j = 0; j < nrow; j++) //move down the rows of eventSegs
+		{
+			//eventSegs is 4 column matrix, node is in first column stored as double
+			node = (int) REAL(eventSegs)[j + nrow * 0];
+			
+			//event index is in fourth column stored as double
+			event = (int) REAL(eventSegs)[j  + nrow * 3];
+			
+			//begin and end of current branch segment are in second and third columns stored as doubles
+			begin = REAL(eventSegs)[j + nrow * 1];
+			end = REAL(eventSegs)[j + nrow * 2];
+			
+			//find next node to later check for shift point on branch
+			if (j < nrow)
+			{
+				nnode = (int) REAL(eventSegs)[(j+1) + nrow * 0];
+				nxtevent = (int) REAL(eventSegs)[(j+1) + nrow * 3];
+			}
+			
+			//eventData is dataframe holding event parameters for the current tree
+			//need to find the row that corresponds to the event index. in eventData
+			//the rows are strictly ordered such that row 0 = event1, row 1 = event2, etc.
+			Start = REAL(getListElement(eventData, "time"))[event-1];
+			lam1 = REAL(getListElement(eventData, "lam1"))[event-1];
+			lam2 = REAL(getListElement(eventData, "lam2"))[event-1];
+						
+			//need to find which approximating segments match this branch segment
+			//these are passed in strict order so we only need to search top to bottom
+			//and can ignore everything we've been over already
+			for (l = place_holder; l < nsegs; l++)
+			{
+				if ( (int) REAL(nodeseg)[l] == node)
+				{
+					//isGoodStart = REAL(segbegin)[l] >= begin;
+					isGoodStart = ( (REAL(segbegin)[l] - begin) >= 0. || ( (REAL(segbegin)[l] - begin) < 0. && (REAL(segbegin)[l] - begin) >= -1.*eps) );
+					//isGoodEnd = REAL(segend)[l] <= end;
+					isGoodEnd =  ( (REAL(segend)[l] - end) <= 0. || ( (REAL(segend)[l] - end) > 0. && (REAL(segend)[l] - end) <= eps) );
+
+					if (isGoodStart && isGoodEnd)
+					{					
+						relStart = REAL(segbegin)[l] - Start;
+						relEnd = REAL(segend)[l] - Start;
+						ret = getMeanRateExponential(relStart,relEnd,lam1,lam2);
+						
+						REAL(rates)[l] += ret/((double) nsamples);
+					}
+					//check for shift straddle
+					if (node == nnode)
+					{
+						isGoodStart = REAL(segbegin)[l] < end;
+						isGoodEnd = REAL(segend)[l] > end;
+						if (isGoodStart && isGoodEnd)
+						{	
+							relStart = REAL(segbegin)[l] - Start;
+							relEnd = end - Start;
+							leftshift = getTimeIntegratedBranchRate(relStart,relEnd,lam1,lam2);
+							
+							relStart = 0.;
+							relEnd = REAL(segend)[l] - end;
+							lam1 = REAL(getListElement(eventData, "lam1"))[nxtevent-1];
+							lam2 = REAL(getListElement(eventData, "lam2"))[nxtevent-1];
+							rightshift = getTimeIntegratedBranchRate(relStart,relEnd,lam1,lam2);
+							
+							ret = (leftshift+rightshift)/(REAL(segend)[l] - REAL(segbegin)[l]);
+							
+							REAL(rates)[l] += ret/((double) nsamples);
+							place_holder = l; place_holder++;
+							break;
+						}
+					}
+				}
+				else
+				{
+					place_holder = l;
+					break;
+				}
+			}			
+		}
+		UNPROTECT(2); nprotect -= 2; //protected eventSegs and eventData, which we no longer need
+	}
+	UNPROTECT(nprotect);
+	return rates;
+}
---
+++ Rcode/src/treetraverse.c
@@ -0,0 +1,200 @@
+#include <R.h>
+
+void postorder_tree_traverse(int * anc, int * desc, int * node, int * nnode, int * order);
+void preorder_tree_traverse(int * anc, int * desc, int * node, int * nnode, int * order);
+void rootward(int * anc, int * desc, int * node, int * nnode, int * ndorder);
+void tipward(int * anc, int * desc, int * node, int * nnode, int * ndorder);
+void setpolartreecoords(int * anc, int * desc, int * ndorder, int * ntip, int * rootnd, int * nnode, double * ths, double * theta, double * root);
+void setphylotreecoords(int * anc, int * desc, int * ndorder, double * bl, int * ntip, int * rootnd, int * nnode, double * bar, double * xy, double * root);
+
+static int zzz;
+
+void postorder_tree_traverse(int * anc, int * desc, int * node, int * nnode, int * ndorder)
+{
+	zzz = 0;
+	rootward(anc, desc, node, nnode, ndorder);
+}
+
+void preorder_tree_traverse(int * anc, int * desc, int * node, int * nnode, int * ndorder)
+{
+	zzz = 0;
+	tipward(anc, desc, node, nnode, ndorder);
+}
+
+void rootward(int * anc, int * desc, int * node, int * nnode, int * ndorder)
+{
+	int i, d = 0;
+	int * children;
+	children = Calloc(2, int);
+	for (i = 0; i < (*nnode-1); i++)
+	{
+		if (anc[i] == *node)
+		{
+			children[d] = desc[i];
+			d++;
+		}
+		if (d == 2) break;
+	}
+	
+	if (children[0] != 0 && children[1] != 0)
+	{
+		int * child;
+		child = Calloc(1,int); 
+		for (i = 0; i < 2; i++)
+		{
+			*child = children[i];
+			rootward(anc, desc, child, nnode, ndorder);
+			//Free(child);
+		}
+		Free(child);
+	}
+	ndorder[zzz] = *node; zzz++;
+	//Rprintf("%d\n", zzz);
+	Free(children);
+}
+
+void tipward(int * anc, int * desc, int * node, int * nnode, int * ndorder)
+{
+	ndorder[zzz] = *node; zzz++;
+	int i, d = 0;
+	int * children;
+	children = Calloc(2, int);
+	for (i = 0; i < (*nnode-1); i++)
+	{
+		if (anc[i] == *node)
+		{
+			children[d] = desc[i];
+			d++;
+		}
+		if (d == 2) break;
+	}
+	
+	if (children[0] != 0 && children[1] != 0)
+	{
+		int * child;
+		child = Calloc(1,int); 
+		for (i = 0; i < 2; i++)
+		{
+			*child = children[i];
+			tipward(anc, desc, child, nnode, ndorder);
+			//Free(child);
+		}
+		Free(child);
+	}
+	//Rprintf("%d\n", zzz);
+	Free(children);
+}
+
+void setpolartreecoords(int * anc, int * desc, int * ndorder, int * ntip, int * rootnd, int * nnode, double * ths, double * theta, double * root)
+{
+	int i, j, d, ne = *nnode - 1;
+	int * ci;
+	ci = Calloc(2, int);
+	
+	for (i = 0; i < *nnode; i++)
+	{
+		d = 0;
+		if (ndorder[i] <= *ntip)
+		{
+			for (j = 0; j < ne; j++)
+			{
+				if (desc[j] == ndorder[i])
+				{
+					theta[j + ne * 0] = (ndorder[i] - 1) * (*ths);
+					theta[j + ne * 1] = (ndorder[i] - 1) * (*ths);
+					theta[j + ne * 2] = (ndorder[i] - 1) * (*ths); 
+					break;
+				}
+			}
+		}
+		else
+		{
+			for (j = 0; j < ne; j++)
+			{
+				if (anc[j] == ndorder[i])
+				{
+					ci[d] = j;
+					d++;
+				}
+				if (d == 2) break;
+			}
+			if (ndorder[i] == *rootnd)
+			{
+				root[0] = theta[ci[0] + ne * 0]/2. + theta[ci[1] + ne * 0]/2.;
+				root[1] = theta[ci[0] + ne * 0];
+				root[2] = theta[ci[1] + ne * 0];
+				continue;
+			}
+			for (j = 0; j < ne; j++) if (desc[j] == ndorder[i]) break;
+			theta[j + ne * 0] = theta[ci[0] + ne * 0]/2. + theta[ci[1] + ne * 0]/2.;
+			theta[j + ne * 1] = theta[ci[0] + ne * 0];
+			theta[j + ne * 2] = theta[ci[1] + ne * 0];
+		}
+	}
+	Free(ci);
+}
+
+void setphylotreecoords(int * anc, int * desc, int * ndorder, double * bl, int * ntip, int * rootnd, int * nnode, double * bar, double * xy, double * root)
+{
+	int i, j, d, ne = *nnode - 1;
+	double dy = 1./(*ntip);
+	int * ci;
+	ci = Calloc(2, int);
+	
+	for (i = 0; i < *nnode; i++)
+	{
+		d = 0;
+		if (ndorder[i] <= *ntip)
+		{
+			for (j = 0; j < ne; j++)
+			{
+				if (desc[j] == ndorder[i])
+				{
+					xy[j + ne * 0] = 1. - bl[j];
+					xy[j + ne * 1] = (ndorder[i] - 1) * dy;
+					xy[j + ne * 2] = 1.;
+					xy[j + ne * 3] = (ndorder[i] - 1) * dy;
+					
+					bar[j + ne * 0] = 0.;
+					bar[j + ne * 1] = 0.;
+					bar[j + ne * 2] = 0.;
+					bar[j + ne * 3] = 0.;
+
+					break;
+				}
+			}
+		}
+		else
+		{
+			for (j = 0; j < ne; j++)
+			{
+				if (anc[j] == ndorder[i])
+				{
+					ci[d] = j;
+					d++;
+				}
+				if (d == 2) break;
+			}
+			if (ndorder[i] == *rootnd)
+			{
+				root[0] = xy[ci[0] + ne * 0];
+				root[1] = xy[ci[0] + ne * 3];
+				root[2] = xy[ci[0] + ne * 0];
+				root[3] = xy[ci[1] + ne * 3];
+				continue;
+			}
+			for (j = 0; j < ne; j++) if (desc[j] == ndorder[i]) break;
+			
+			xy[j + ne * 0] = xy[ci[0] + ne * 0] - bl[j];
+			xy[j + ne * 1] = xy[ci[0] + ne * 3]/2. + xy[ci[1] + ne * 3]/2.;
+			xy[j + ne * 2] = xy[ci[0] + ne * 0];
+			xy[j + ne * 3] = xy[ci[0] + ne * 3]/2. + xy[ci[1] + ne * 3]/2.;
+					
+			bar[j + ne * 0] = xy[ci[0] + ne * 0];
+			bar[j + ne * 1] = xy[ci[0] + ne * 3];
+			bar[j + ne * 2] = xy[ci[1] + ne * 0];
+			bar[j + ne * 3] = xy[ci[1] + ne * 3];
+		}
+	}
+	Free(ci);
+}
