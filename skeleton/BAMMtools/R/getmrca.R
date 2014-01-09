1ab2ebd046f115358455562985610b49ce079ab1
blueraleigh <mcgrundler@gmail.com>
2014-01-09 16:35:27 -0500

    Adding documentation

--- skeleton/BAMMtools/R/getmrca.R
+++ skeleton/BAMMtools/R/getmrca.R
@@ -1,5 +1,11 @@
 getmrca = function(phy,t1,t2)
 {
+	if (mode(t1) == "character") {
+		t1 = match(t1, phy$tip.label);
+	}
+	if (mode(t2) == "character") {
+		t2 = match(t2, phy$tip.label);
+	}
 	ne = as.integer(dim(phy$edge)[1]);
 	npair = as.integer(length(t1));
 	anc = as.integer(phy$edge[,1]);
