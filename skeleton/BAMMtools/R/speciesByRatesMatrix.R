421bbf34adadf59e936ee18f50678598419bb9e3
blueraleigh <mcgrundler@gmail.com>
2014-01-24 12:27:17 -0500

    New function speciesByRatesMatrix.R

---
+++ skeleton/BAMMtools/R/speciesByRatesMatrix.R
@@ -0,0 +1,47 @@
+speciesByRatesMatrix = function(ephy, nslices, index, spex = "s") {
+	phy = as.phylo.bammdata(ephy);
+	seq.nod = .Call("seq_root2tip", phy$edge, length(phy$tip.label), phy$Nnode, PACKAGE = "ape");
+	if (nslices > length(phy$tip.label)) {
+		warning("You are generating more time slice variables than species. Consider choosing a smaller number of slices");
+	}
+	if (nslices <= 100) {
+		tvec = seq(0, 1, 0.01) * max(branching.times(phy));
+		tvec = tvec[seq.int(1,length(tvec),length.out=nslices+1)];
+		m = dtRates(ephy, 0.01, index, tmat = TRUE);
+	}
+	else if (nslices > 100 && nslices <= 500) {
+		tvec = seq(0, 1, 0.002) * max(branching.times(phy));
+		tvec = tvec[seq.int(1,length(tvec),length.out=nslices+1)];
+		m = dtRates(ephy, 0.002, index, tmat = TRUE);
+	}
+	else if (nslices > 500 && nslices <= 1000) {
+		tvec = seq(0, 1, 0.001) * max(branching.times(phy));
+		tvec = tvec[seq.int(1,length(tvec),length.out=nslices+1)];
+		m = dtRates(ephy, 0.001, index, tmat = TRUE);
+	}
+	else {
+		stop("Max slices (1000) exceeded.  Choose a smaller number of slices");
+	}
+	tol = 0.0001*max(branching.times(phy));
+	ret = lapply(seq.nod, function(x) {
+		path = which(m$dtrates$tmat[,1] %in% x);
+		ids = unlist(sapply(tvec[-length(tvec)], function(y) which(abs(m$dtrates$tmat[path,2] - y) < tol)[1]));
+		if (ephy$type == "trait") {
+			return(m$dtrates$rates[ids]);
+		}
+		else {
+			if (tolower(spex) == "s") {
+				return(m$dtrates$rates[[1]][ids]);
+			}
+			else if (tolower(spex) == "e") {
+				return(m$dtrates$rates[[2]][ids]);
+			}
+			else {
+				return(m$dtrates$rates[[1]][ids] - m$dtrates$rates[[2]][ids]);
+			}
+		}
+	})
+	ret = do.call(rbind, ret);
+	rownames(ret) = phy$tip.label;
+	return(ret);	
+}
