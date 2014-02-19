108cd1079c71c423c36aa67be83aafa80151c0f8
blueraleigh <mcgrundler@gmail.com>
2014-02-19 13:30:46 -0500

    Add tol value to rate functions

--- skeleton/BAMMtools/R/timeIntegratedBranchRate.R
+++ skeleton/BAMMtools/R/timeIntegratedBranchRate.R
@@ -3,16 +3,31 @@
 #	timeIntegratedBranchRate(....)
 #		computes the integral of rates on a branch segment with respect to time
 #		Not the average.
-#		Integrates the exponential function p1 * exp(p2 * t)
+#		
 #		
 
 timeIntegratedBranchRate <- function(t1, t2, p1, p2){
+	tol <- 0.00001;
 	res <- vector(mode = 'numeric', length = length(t1));
-	zero <- which(p2 == 0);
-	res[zero] <- p1[zero] * (t2[zero] - t1[zero]);
-	nonzero <- which(p2 < 0);
-	res[nonzero] <- (p1[nonzero]/p2[nonzero])*(exp(p2[nonzero]*t2[nonzero]) - exp(p2[nonzero]*t1[nonzero]));
-	nonzero <- which(p2 > 0);
-	res[nonzero] <- (p1[nonzero]/p2[nonzero])*(2*p2[nonzero]*(t2[nonzero]-t1[nonzero]) + exp(-p2[nonzero]*t2[nonzero]) - exp(-p2[nonzero]*t1[nonzero]));
+	# constant rate
+	zero <- which(abs(p2) < tol);
+	p1s <- p1[zero];
+	t1s <- t1[zero];
+	t2s <- t2[zero];
+	res[zero] <- p1s * (t2s - t1s);
+	# declining rate
+	nonzero <- which(p2 < -tol);
+	p1s <- p1[nonzero];
+	p2s <- p2[nonzero];
+	t1s <- t1[nonzero];
+	t2s <- t2[nonzero];
+	res[nonzero] <- (p1s/p2s)*(exp(p2s*t2s) - exp(p2s*t1s));
+	# increasing rate
+	nonzero <- which(p2 > tol);
+	p1s <- p1[nonzero];
+	p2s <- p2[nonzero];
+	t1s <- t1[nonzero];
+	t2s <- t2[nonzero];
+	res[nonzero] <- (p1s/p2s)*(2*p2s*(t2s-t1s) + exp(-p2s*t2s) - exp(-p2s*t1s));
 	return(res);
 }
