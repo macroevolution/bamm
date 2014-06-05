ed96760d0597857be0187ed6137b70190eea52a3
Carlos Anderson <carlosjanderson@gmail.com>
2014-06-05 11:08:33 -0400

    Create TimeModeProposal as a base class
    LambdaTimeModeProposal now derives from TimeModeProposal and was
    updated appropriately. BetaTimeModeProposal will also derive from
    TimeModeProposal in the future.

--- src/LambdaTimeModeProposal.h
+++ src/LambdaTimeModeProposal.h
@@ -2,64 +2,30 @@
 #define LAMBDA_TIME_MODE_PROPOSAL_H
 
 
-#include "Proposal.h"
-#include "Prior.h"
+#include "TimeModeProposal.h"
 
 class Random;
 class Settings;
 class Model;
-class Tree;
-class SpExBranchEvent;
 
 
-class LambdaTimeModeProposal : public Proposal
+class LambdaTimeModeProposal : public TimeModeProposal
 {
-    enum ProposalType {
-        TimeConstant,
-        TimeVariable
-    };
-
 public:
 
     LambdaTimeModeProposal(Random& random, Settings& settings, Model& model);
 
-    virtual void propose();
-    virtual void accept();
-    virtual void reject();
+protected:
 
-    virtual double acceptanceRatio();
+    double initialParameter(BranchEvent* event);
+    double rateParameter(BranchEvent* event);
+    bool isTimeVariable(BranchEvent* event);
+    
+    void setEventParameters(BranchEvent* event,
+        double initParam, double rateParam, bool isTimeVariable);
 
-private:
+    double rateParameterFromPrior();
 
-    void makeTimeConstant(SpExBranchEvent* event);
-    void makeTimeVariable(SpExBranchEvent* event);
-
-    double computeMeanLambda(double lam0, double k, double T);
-    double computeLambdaInit(double lam, double k, double T);
-
-    double computeLogLikelihoodRatio();
-    double computeLogPriorRatio();
-    double computeLogJacobian();
-    double computeJacobian(double k, double T);
-
-    Model& _model;
-    Tree* _tree;
-
-    Prior _prior;
-
-    SpExBranchEvent* _event;
-
-    double _currentLambdaInit;
-    double _currentLambdaShift;
-    bool _currentIsTimeVariable;
-
-    double _currentLogLikelihood;
-    double _currentLogPrior;
-
-    double _proposedLogLikelihood;
-    double _proposedLogPrior;
-
-    ProposalType _lastTimeModeProposal;
 };
 
 
