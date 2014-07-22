053fd41c6815b9932c5e7375a87d85f6291bfa5c
Carlos Anderson <carlosjanderson@gmail.com>
2014-07-22 12:21:30 -0400

    Print more information in chain swap percent tool

--- tools/chain-swap-percent.sh
+++ tools/chain-swap-percent.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# Print the chain swap percentage for the given run
+# Print the chain swap percentage, accepted, and total for the given run
 # e.g., chain-swap-percent ./bamm -c divcontrol.txt --numberOfGenerations 1000
 # Can use all BAMM options except --outName and any option
 #     that changes the name of an output file
+# Applies a 20% burn-in
 
 set -e    # Exit on any error
 set -u    # Report unbound variables
 
 if [ $# = 0 ]
 then
+    echo Prints the percentage of chain swaps accepted, the number accepted,
+    echo and the number proposed \(space-delimited\) after a 20% burn-in.
     echo Usage: chain-swap-percent [bamm_path] [bamm_options]
+    exit
 fi
 
 outName=chain-swap-percent-$RANDOM
@@ -30,7 +34,9 @@ $@ --outName $outName $fileNameOptions > /dev/null
 
 realChainSwapFileName=${outName}_$chainSwapFileName
 
-Rscript -e "cat(mean(read.csv(\"$realChainSwapFileName\")\$swapAccepted)); \
-    cat(\"\\\n\")"
+Rscript -e "data = read.csv(\"$realChainSwapFileName\")\$swapAccepted; \
+            data = tail(data, 0.8 * length(data)); \
+            cat(paste(mean(data), sum(data), length(data))); \
+            cat(\"\\\n\")"
 
-rm $outName*
+rm ${outName}*
