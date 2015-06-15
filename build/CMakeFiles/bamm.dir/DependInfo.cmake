4e35fdf4ec479c00bf23a16ff137a393b4d173a6
ptitle <pascaltitle@gmail.com>
2015-06-15 17:15:44 -0400

    web documentation update

---
+++ build/CMakeFiles/bamm.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/pascaltitle/bamm/src/AcceptanceDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/AcceptanceDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/BetaInitProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/BetaInitProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/BetaShiftProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/BetaShiftProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/BetaTimeModeProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/BetaTimeModeProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/BranchEvent.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/BranchEvent.cpp.o"
+  "/Users/pascaltitle/bamm/src/BranchHistory.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/BranchHistory.cpp.o"
+  "/Users/pascaltitle/bamm/src/ChainSwapDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/ChainSwapDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/CommandLineProcessor.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/CommandLineProcessor.cpp.o"
+  "/Users/pascaltitle/bamm/src/EventCountLog.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/EventCountLog.cpp.o"
+  "/Users/pascaltitle/bamm/src/EventDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/EventDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/EventNumberForBranchProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/EventNumberForBranchProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/EventNumberProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/EventNumberProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/EventParameterProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/EventParameterProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/EventRateProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/EventRateProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/FastSimulatePrior.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/FastSimulatePrior.cpp.o"
+  "/Users/pascaltitle/bamm/src/LambdaInitProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/LambdaInitProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/LambdaShiftProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/LambdaShiftProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/LambdaTimeModeProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/LambdaTimeModeProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/Log.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Log.cpp.o"
+  "/Users/pascaltitle/bamm/src/MCMC.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MCMC.cpp.o"
+  "/Users/pascaltitle/bamm/src/MCMCDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MCMCDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/MbRandom.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MbRandom.cpp.o"
+  "/Users/pascaltitle/bamm/src/MetropolisCoupledMCMC.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MetropolisCoupledMCMC.cpp.o"
+  "/Users/pascaltitle/bamm/src/Model.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Model.cpp.o"
+  "/Users/pascaltitle/bamm/src/ModelDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/ModelDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/MoveEventProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MoveEventProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/MuInitProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MuInitProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/MuShiftProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/MuShiftProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/NewickTreeReader.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/NewickTreeReader.cpp.o"
+  "/Users/pascaltitle/bamm/src/Node.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Node.cpp.o"
+  "/Users/pascaltitle/bamm/src/NodeStateDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/NodeStateDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/NodeStateProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/NodeStateProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/PreservationRateProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/PreservationRateProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/Prior.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Prior.cpp.o"
+  "/Users/pascaltitle/bamm/src/Proposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Proposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/Random.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Random.cpp.o"
+  "/Users/pascaltitle/bamm/src/Settings.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Settings.cpp.o"
+  "/Users/pascaltitle/bamm/src/SettingsParameter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/SettingsParameter.cpp.o"
+  "/Users/pascaltitle/bamm/src/SpExBranchEvent.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/SpExBranchEvent.cpp.o"
+  "/Users/pascaltitle/bamm/src/SpExDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/SpExDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/SpExEventDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/SpExEventDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/SpExModel.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/SpExModel.cpp.o"
+  "/Users/pascaltitle/bamm/src/Stat.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Stat.cpp.o"
+  "/Users/pascaltitle/bamm/src/StdOutDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/StdOutDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/TimeModeProposal.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/TimeModeProposal.cpp.o"
+  "/Users/pascaltitle/bamm/src/Tools.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Tools.cpp.o"
+  "/Users/pascaltitle/bamm/src/TraitBranchEvent.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/TraitBranchEvent.cpp.o"
+  "/Users/pascaltitle/bamm/src/TraitDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/TraitDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/TraitEventDataWriter.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/TraitEventDataWriter.cpp.o"
+  "/Users/pascaltitle/bamm/src/TraitModel.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/TraitModel.cpp.o"
+  "/Users/pascaltitle/bamm/src/Tree.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/Tree.cpp.o"
+  "/Users/pascaltitle/bamm/src/main.cpp" "/Users/pascaltitle/bamm/build/CMakeFiles/bamm.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "BAMM_VERSION=\"2.3.1\""
+  "BAMM_VERSION_DATE=\"2015-05-20\""
+  "GIT_COMMIT_ID=\"eb233a289156ad0d35384f920483c9919f01cbf5\""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
